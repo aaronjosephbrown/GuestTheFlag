b7b44d86d4bcc471afd8f2ce3ab56c20de725b75
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-09-27 17:13:06 -0500

    Initial Commit

---
+++ GuestTheFlag/GuestTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestTheFlagApp.swift
+//  GuestTheFlag
+//
+//  Created by Aaron Brown on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GuestTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
